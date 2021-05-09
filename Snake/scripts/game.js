183d7f4fa25b136e67471965570c11c3d773b5e7
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2021-05-09 10:47:55 -0300

    :cactus: Game Basics

--- Snake/scripts/game.js
+++ Snake/scripts/game.js
@@ -1,22 +1,27 @@
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from "./snake.js";
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 20;
+const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
 
-  if(secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
 
   lastRenderTime = currentTime;
+
+  update();
+  draw();
 }
 
 window.requestAnimationFrame(main);
 
-
 function update() {
-  
+  updateSnake();
 }
 
 function draw() {
-  
+  gameBoard.innerHTML = '';
+  drawSnake(gameBoard);
 }
--- Snake/scripts/snake.js
+++ Snake/scripts/snake.js
@@ -1,1 +1,28 @@
-export const SNAKE_SPEED = 20;
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 10;
+const snakeBody = [
+  { x: 10, y: 11 },
+  { x: 11, y: 11 },
+  { x: 12, y: 11 },
+];
+
+export function update() {
+  const inputDirection = getInputDirection();
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
+}
+
+export function draw(gameBoard) {
+  snakeBody.map((segment) => {
+    const snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = segment.y
+    snakeElement.style.gridColumnStart = segment.x
+    snakeElement.classList.add("snake");
+    gameBoard.appendChild(snakeElement);
+  });
+}
