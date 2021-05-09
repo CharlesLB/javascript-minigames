647d2e047ce00738a1687f79d7f9d5b72e1c88dd
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2021-05-09 11:07:14 -0300

    :cactus: Game

--- Snake/scripts/snake.js
+++ Snake/scripts/snake.js
@@ -1,16 +1,12 @@
 import { getInputDirection } from "./input.js";
 
-export const SNAKE_SPEED = 10;
-
-const snakeBody = [
-  { x: 10, y: 11 },
-  { x: 11, y: 11 },
-  { x: 12, y: 11 },
-];
-
+export const SNAKE_SPEED = 5;
+const snakeBody = [{ x: 11, y: 11 }];
 let newSegments = 0;
 
 export function update() {
+  addSegments();
+
   const inputDirection = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
@@ -34,12 +30,29 @@ export function expandSnake(amount) {
   newSegments += amount;
 }
 
-export function onSnake(position) {
-  return snakeBody.some((segment) => {
+export function onSnake(position, { ignoreHead = false } = {}) {
+  return snakeBody.some((segment, index) => {
+    if (ignoreHead && index === 0) return false;
     return equalPositions(segment, position);
   });
 }
 
+export function getSnakeHead() {
+  return snakeBody[0];
+}
+
+export function snakeIntersection() {
+  return onSnake(snakeBody[0], { ignoreHead: true });
+}
+
 function equalPositions(pos1, pos2) {
   return pos1.x === pos2.x && pos1.y === pos2.y;
+}
+
+function addSegments() {
+  for (let i = 0; i < newSegments; i++) {
+    snakeBody.push({ ...snakeBody[snakeBody.length - 1] });
+  }
+
+  newSegments = 0;
 }
